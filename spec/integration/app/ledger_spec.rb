c3677fd0663710022bc2e5815c0447b456cc237a
Osnyder12 <snyderowen12@gmail.com>
2023-12-20 11:56:02 -0500

    finished under failed example page 87

--- spec/integration/app/ledger_spec.rb
+++ spec/integration/app/ledger_spec.rb
@@ -16,5 +16,19 @@ module ExpenseTracker
     describe '#record' do
     # context
     end
+
+    context 'with a valid expense' do
+      it 'successfully saves the expense in the DB' do
+        result = ledger.record(expense)
+
+        expect(result).to be_success
+        expect(DB[:expenses].all).to match [a_hash_including(
+          id: result.expense_id,
+          payee: 'Starbucks',
+          amount: 5.75,
+          date: Date.iso8601('2017-06-10')
+        )]
+      end
+    end
   end
 end
